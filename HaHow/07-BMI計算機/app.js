a70ef72fcfb38ecd99a28fc87f0d45e2e25e9d5f
RealMan <ethan1126.ilink@gmail.com>
2023-03-01 18:55:02 +0800

    Update app.js

--- HaHow/07-BMI計算機/app.js
+++ HaHow/07-BMI計算機/app.js
@@ -4,18 +4,18 @@ function BMI_Calculus(h, w){
     return w / (h**2)
 }
 
-document.addEventListener('DOMContentLoaded', () =>{
+
+document.addEventListener('DOMContentLoaded', () => {
     let height = document.querySelector('#bodyHeight')
     let weight = document.querySelector('#bodyWeight')
     let bmi_res = document.querySelector('#resultText')
     let btn = document.querySelector('button')
-
-    btn.addEventListener('click', () => {
-        let h = Number(height.value) /100
-        let w = Number(weight.value)
-        bmi_res.textContent = BMI_Calculus(h, w).toFixed(2) // 取到小數點第二位, 但會強制轉成字串
-    })
-   
+    
+        btn.addEventListener('click', () => {
+            let h = Number(height.value) /100
+            let w = Number(weight.value)
+            bmi_res.textContent = BMI_Calculus(h, w).toFixed(2) // 取到小數點第二位, 但會強制轉成字串
+        })
 })
 
 
